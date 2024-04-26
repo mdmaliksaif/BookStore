321fc984411804116ffe8e644bc21426f507cc0a
Saif Malik <saifmalik10786@gmail.com>
2024-04-26 23:21:15 +0530

    first commit

---
+++ Frontend/src/components/About.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Navbar from './Navbar'
+import Footer from './Footer'
+
+const About = () => {
+  return (
+    <div className='text-2xl text-white'>
+        <Navbar/>
+        {/* Heading */}
+        <div className='ml-72'>
+            <h1 className=' pt-36  text-green-400'>Who We Are</h1>
+            <p className='mr-36 mt-12 text-justify'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Non animi mollitia, odio cum voluptas hic pariatur, distinctio veniam unde tenetur voluptatem beatae velit sed. Veniam laborum cumque aspernatur! Assumenda, accusantium?</p>
+        </div>
+
+        <div className='mt-20 md:flex  '>
+            <div><img src="https://cdn.pixabay.com/photo/2015/11/19/21/10/glasses-1052010_640.jpg" className='' alt="" /></div>
+            <div className='p-10 -mt-10'>
+                <h1 className='text-blue-400'>Our History</h1>
+                <div className='w-[800px] '><p className='mr-24'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta animi, accusamus nisi corrupti rem fugit sint necessitatibus aut similique et obcaecati perferendis iure eligendi porro? Nostrum porro commodi similique fugiat!
+            Modi vel doloremque dignissimos qui assumenda. Qui quaerat id soluta modi molestias commodi quae sunt quidem, aperiam, est voluptates, iure sed aliquid dolores mollitia ratione sapiente laudantium fugiat itaque. Sunt!
+          um sit deleniti inventore, numquam aut laborum officia ipsam obcaecati ipsa voluptate eveniet nulla animi fugit voluptatibus accusantium? Reprehenderit illum perferendis sed.</p></div>
+        </div>
+            </div>
+            <Footer/>
+    </div>
+  )
+}
+
+export default About
---
+++ Frontend/src/components/Meet.jsx
@@ -0,0 +1,67 @@
+import React from 'react'
+import Navbar from './Navbar'
+import Footer from './Footer'
+
+const Meet = () => {
+  return (
+    <>
+    <Navbar/>
+     <div className='flex   p-44'>
+        {/* left */}
+        <div className='w-1/2'><img src="https://www.shutterstock.com/image-photo/smiling-african-american-customer-care-600nw-2237515137.jpg" alt="" /></div>
+        {/* right */}
+        <div className='w-1/2 ml-20 relative'>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+      <div className="flex items-center">
+        <label htmlFor="firstName" className="mr-2 block text-sm font-medium text-white">
+          First Name:
+        </label>
+        <input
+          type="text"
+          id="firstName"
+          name="firstName"
+          className="w-full px-3 py-2 rounded-md border-2 bg-transparent text-white border-gray-300 focus:outline-none focus:ring-1 focus:ring-blue-500"
+        />
+      </div>
+      <div className="flex items-center">
+        <label htmlFor="lastName" className="mr-2 block text-sm font-medium text-white">
+          Last Name:
+        </label>
+        <input
+          type="text"
+          id="lastName"
+          name="lastName"
+          className="w-full px-3 py-2 rounded-md border-2 bg-transparent  text-white border-gray-300 focus:outline-none focus:ring-1 focus:ring-blue-500"
+        />
+      </div>
+      <div className="col-span-full">
+        <label htmlFor="email" className="block text-sm font-medium text-white">
+          Email:
+        </label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          className="w-full px-3 py-2 rounded-md border-2 bg-transparent text-white border-gray-300 focus:outline-none focus:ring-1 focus:ring-blue-500"
+        />
+      </div>
+      <div className="col-span-full">
+        <label htmlFor="feedback" className="block text-sm font-medium text-white">
+          Leave few Words:
+        </label>
+        <textarea
+          id="feedback"
+          name="feedback"
+          className="w-full h-36 px-3 py-2 rounded-md border-2 bg-transparent text-white border-gray-300 focus:outline-none focus:ring-1 focus:ring-blue-500"
+        />
+        <button className='bg-red-500 px-4 py-2 text-black rounded-md'>Submit</button>
+      </div>
+    </div>
+        </div>
+   </div>
+   <Footer/>
+    </>
+  )
+}
+
+export default Meet
